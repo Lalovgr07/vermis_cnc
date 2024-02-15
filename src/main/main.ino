7039738515f0ffff00bca66786417a31b6d2a51a
Lalovgr07 <126952732+Lalovgr07@users.noreply.github.com>
2024-02-15 19:06:51 +0100

    denoise stepper readings

--- src/main/main.ino
+++ src/main/main.ino
@@ -3,19 +3,24 @@
 #define PIN_SW 2
 #include <Stepper.h>
 
-const int stepsPerRevolution = 200;  // change this to fit the number of steps per revolution
+const int stepsPerRevolution = 200; 
+// change this to fit the number of steps per revolution
 
 // initialize the stepper library on pins 8 through 11:
 Stepper myStepperX(stepsPerRevolution, 4, 5, 6, 7);
 Stepper myStepperY(stepsPerRevolution, 8, 9, 10, 11);
 
-int stepCount = 0;  // number of steps the motor has taken
+int stepCount = 0;  
+// number of steps the motor has taken
 
 void setup() {
+
+    Serial.begin(9600);
+    
     int motorSpeed = 10;
+    
     myStepperX.setSpeed(motorSpeed);
     myStepperY.setSpeed(motorSpeed);
-    Serial.begin(9600);
 
     pinMode( PIN_SW, INPUT_PULLUP );
 
@@ -25,6 +30,13 @@ void loop() {
 
     int stepsX = map(analogRead(PIN_VRx), 0, 1023, -5, 5 );
     int stepsY = map(analogRead(PIN_VRy), 0, 1023, -5, 5 );
+
+    if (stepsX == -1 || stepsX == +1) {
+        stepsX = 0;
+    }
+    if (stepsY == -1 || stepsY == +1) {
+        stepsY = 0;
+    }
     
     myStepperX.step(stepsX);
     myStepperY.step(stepsY);
@@ -32,12 +44,12 @@ void loop() {
      Serial.print( "X_ang:" );
      Serial.print(stepsX );
      Serial.print("  ");
-
      Serial.print( "Y_ang:" );
      Serial.print(stepsY);
      Serial.print( " SW:");
      Serial.print( digitalRead(PIN_SW) );
      Serial.println();
+     
      delay(50);
      
      
